f456ff39f61291ef4e1f312a893172476cb74c5f
Class01 <Class01@DESKTOP-ILL1MFM>
2023-07-14 09:54:02 +0900

    Day230713

---
+++ MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -0,0 +1,76 @@
+package common;
+
+import java.sql.*;
+import javax.servlet.*;
+
+public class JDBConnect {
+	public Connection con;
+	public Statement stmt;
+	public PreparedStatement psmt;
+	public ResultSet rs;
+
+	// 기본 생성자
+	public JDBConnect() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String id = "c##musthave";
+			String pwd = "1234";
+			con = DriverManager.getConnection(url, id, pwd);
+
+			System.out.println("DB연결 성공(기본생성자)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 두 번째 생성자
+	public JDBConnect(String driver, String url, String id, String pwd) {
+		try {
+			Class.forName(driver);
+
+			con = DriverManager.getConnection(url, id, pwd);
+
+			System.out.println("DB 연결 성공(인수 생성자 1)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 세 번째 생성자
+	public JDBConnect(ServletContext application) {
+		try {
+			String driver = application.getInitParameter("OracleDriver");
+			Class.forName(driver);
+
+			String url = application.getInitParameter("OracleURL");
+			String id = application.getInitParameter("OracleId");
+			String pwd = application.getInitParameter("OraclePwd");
+			con = DriverManager.getConnection(url, id, pwd);
+
+			System.out.println("DB 연결 성고 (인수 생성자 2)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 연결 해제(자원 반납)
+	public void close() {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			if (psmt != null)
+				psmt.close();
+			if (con != null)
+				con.close();
+
+			System.out.println("JDBC 자원 해제");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
